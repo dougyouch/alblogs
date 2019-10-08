b3b782d695f1aeefeb85b66be31db25296342972
Doug Youch <dougyouch@gmail.com>
2019-10-08 17:14:52 -0400

    added Entry.request_uri

--- lib/alblogs/entry.rb
+++ lib/alblogs/entry.rb
@@ -2,12 +2,41 @@ module Alblogs
   class Entry < Struct.new(:line, *::Alblogs::FIELDS.keys)
     REGEXP = Regexp.new(::Alblogs::FIELDS.values.join(' '))
 
+    def request_parts
+      @request_parts ||= request.split(' ', 3)
+    end
+
+    def request_method
+      request_parts[0]
+    end
+
+    def request_url
+      request_parts[1]
+    end
+
+    def request_protocol
+      request_parts[2]
+    end
+
+    def request_uri
+      @request_uri ||= URI(request_url)
+    end
+
     def timestamp
       @timestamp ||= Time.iso8601(self[:timestamp])
     end
 
     def target_processing_time
       self[:target_processing_time].to_f
+    end
+
+    # this is the response code the client received
+    def elb_status_code
+      self[:elb_status_code].to_i
+    end
+
+    def target_status_code
+      self[:target_status_code] == '-' ? nil : self[:target_status_code].to_i
     end
 
     def self.from_line(line)
